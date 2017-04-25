174fd4aeca4f55c8fe7a80058395379686d7aa08
chloeliao7 <676336487@qq.com>
2017-04-25 10:08:01 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
-package Table;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Point;
 
 import javax.swing.JFrame;
@@ -17,21 +18,22 @@ public class Main extends JFrame {
 	public Main() {
 		this.setSize(800, 500);
 		this.setLocation(new Point(400, 200));
-		this.setLayout(null);
+		getContentPane().setLayout(null);
 		this.setTitle("iCourse");
-		Loging = new JLabel("Loging");
-		Loging.setBounds(650, 330, 100, 30);
+		Loging = new JLabel("Sign In");
+		Loging.setBounds(650, 357, 100, 30);
 		SignUp = new JLabel("Sign Up");
-		SignUp.setBounds(650, 380, 100, 30);
+		SignUp.setBounds(650, 399, 100, 30);
 		Course = new JLabel("iCourse");
-		Course.setFont(new java.awt.Font("Andale Mono", 1, 80));
-		Course.setBounds(52, 80, 400, 80);
+		Course.setForeground(new Color(105, 105, 105));
+		Course.setFont(new Font("Futura", Font.BOLD, 80));
+		Course.setBounds(260, 88, 295, 120);
 		listenerForMain myaction = new listenerForMain(this);
 		Loging.addMouseListener(myaction);
 		SignUp.addMouseListener(myaction);
-		this.add(Loging);
-		this.add(SignUp);
-		this.add(Course);
+		getContentPane().add(Loging);
+		getContentPane().add(SignUp);
+		getContentPane().add(Course);
 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
