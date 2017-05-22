624e0852bae4fff805c0b569b59d90dc8324a4bc
chloeliao7 <676336487@qq.com>
2017-05-22 10:00:00 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,20 @@
+package Client;
 
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Point;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class Main extends JFrame {
-	   /*creat some JLabel*/
-	JLabel Loging;
-	JLabel SignUp;
+	JButton Loging;
+	JButton SignUp;
 	JLabel Course;
 
 	public static void main(String[] args) {
 		new Main();
-		//you can use the main method by this way
 	}
 
 	public Main() {
@@ -22,16 +22,14 @@ public class Main extends JFrame {
 		this.setLocation(new Point(400, 200));
 		getContentPane().setLayout(null);
 		this.setTitle("iCourse");
-		Loging = new JLabel("Sign In");
+		Loging = new JButton("Sign In");
 		Loging.setBounds(650, 357, 100, 30);
-		SignUp = new JLabel("Sign Up");
+		SignUp = new JButton("Sign Up");
 		SignUp.setBounds(650, 399, 100, 30);
 		Course = new JLabel("iCourse");
 		Course.setForeground(new Color(105, 105, 105));
 		Course.setFont(new Font("Futura", Font.BOLD, 80));
 		Course.setBounds(260, 88, 295, 120);
-		
-		/*add listener*/
 		listenerForMain myaction = new listenerForMain(this);
 		Loging.addMouseListener(myaction);
 		SignUp.addMouseListener(myaction);
@@ -40,9 +38,8 @@ public class Main extends JFrame {
 		getContentPane().add(Course);
 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		   //when close the window run this operation
 		this.setVisible(true);
-		   //Set window visible
 	}
 
 }
+
