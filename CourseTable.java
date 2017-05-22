d54fa3b61149c20b65ab42ace205a7e702b648c8
chloeliao7 <676336487@qq.com>
2017-05-22 09:57:01 +0800

    Update CourseTable.java

--- CourseTable.java
+++ CourseTable.java
@@ -1,3 +1,12 @@
+package Client;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -7,34 +16,60 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 
-public class CourseTable extends JFrame {
+public class CourseTable extends JFrame implements Runnable {
 	final JScrollPane scrollPane = new JScrollPane();
 	JTable table;
 	JButton save;
-	Course courses[][];
+	String[][] content = new String[5][7];
+	private PrintWriter writer;
+	private BufferedReader reader;
+	private Socket socket;
+
+	private void connect() {
+		try {
+			socket = new Socket("localhost", 1978);
+			writer = new PrintWriter(socket.getOutputStream(), true);// 创建输出流对象
+			reader = new BufferedReader(new InputStreamReader(socket.getInputStream())); // 实例化BufferedReader对象
+			new Thread().start();
+
+		} catch (Exception e) {
+			e.printStackTrace(); // 输出异常信息
+		}
+	}
+
+	public static void main(String[] args) {
+		new CourseTable();
+	}
 
 	public CourseTable() {
-
-		courses = new Course[6][7];
-		for (int i = 0; i < 6; i++) {
+		String[] time = { "8:00~9:30", "9:55~11:30", "13:30~15:00", "15:25~17:00", "19:00~20:30" };
+		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 7; j++) {
-				courses[i][j] = new Course(j, j, " ");
+				while(reader!=null){
+					try {
+						String infor = reader.readLine();
+						// content[i][j] = users.accounts.get(account_id).courses[i][j].getName();
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
 			}
 		}
-
-		save = new JButton("save");
-		save.setBounds(200, 450, 100, 80);
-		this.add(save);
-
-		String[][] content = new String[][] { { "8:00~9:30", " ", " ", " ", " ", " ", " ", " " }, { "9:55~11:30", " ", " ", " ", " ", " ", " ", " " }, { "13:30~15:00", " ", " ", " ", " ", " ", " ", " " }, { "15:25~17:00", " ", " ", " ", " ", " ", " ", " " }, { "19:00~20:30", " ", " ", " ", " ", " ", " ", " " } };
+		for (int i = 0; i < 5; i++) {
+			content[i][0] = time[i];
+		}
 		String[] day = { "Times", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
 
 		DefaultTableModel tmd = new DefaultTableModel(content, day);
 
-		table = new JTable(tmd);
+		table = new JTable(tmd) {
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
 
 		scrollPane.setViewportView(table);
-
 		setTitle("course");
 		setSize(800, 600);
 		setLocation(500, 200);
@@ -48,22 +83,73 @@ public class CourseTable extends JFrame {
 		scrollPane.setViewportView(table);
 		add(scrollPane);
 
-		listenerForTable myaction = new listenerForTable(this);
-		table.getModel().addTableModelListener(myaction);
-		save.addMouseListener(myaction);
+		table.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				int row = table.getSelectedRow();
+				int column = table.getSelectedColumn();
+				new edit(row, column, 1);// ???
+				dispose();
+			}
+		});
 
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		setVisible(true);
-
+		connect();
 	}
 
-	public boolean isCellEditable(int rowIndex, int columnIndex) {
-		return false;
-	}
+	boolean isrun = true;
 
-	public static void main(String[] args) {
-		new CourseTable();
-	}
+	@Override
+	public void run() {
+		try {
+			while (isrun) { // 如果套接字是连接状态
+				if (reader != null) {
+					// new CourseTable();
+					String info = reader.readLine();
 
+					isrun = false;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (reader != null) {
+					reader.close();
+				}
+				if (socket != null) {
+					socket.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
 
