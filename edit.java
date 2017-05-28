5a654a7cf8c3d9373f18f1c7938aad1d7fe26c44
chloeliao7 <676336487@qq.com>
2017-05-28 10:52:11 +0800

    Update edit.java

--- edit.java
+++ edit.java
@@ -1,82 +1,76 @@
-package version2;
+package Client;
 
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-import setting.account;
-
-
-public class edit extends JFrame {
+public class edit extends JFrame implements Runnable {
 	JLabel course_name;
-	//set a form for course name
 	JLabel classroom;
-	//set a form for classroom
 	JLabel lecturer;
-	//set a form for lecturer
 	JButton confirm;
-	//set a button to confirm
 	JButton exit;
-	//set a button to exit
 	JTextField name;
-	//Allow edit single line user name text 
 	JTextField location;
-	//Allow edit single line location text 
 	JTextField teacher;
-	//Allow edit single line teacher text 
-	
-	/*int some values*/
 	int account_id;
 	int row;
 	int column;
-	public edit(int row,int column,int account_id) {
-		this.account_id=account_id;
-		this.row=row;
-		this.column=column;
-		
+	private PrintWriter writer; // 声明PrintWriter类对象
+	private BufferedReader reader; // 声明BufferedReader对象
+	private Socket socket; // 声明Socket对象
+	private String instruction;
+
+	private void connect() { // 连接套接字方法
+		try { // 捕捉异常
+			writer = new PrintWriter(socket.getOutputStream(), true);// 创建输出流对象
+			reader = new BufferedReader(new InputStreamReader(socket.getInputStream())); // 实例化BufferedReader对象
+			new Thread(this).start();
+			// getServerInfo();
+
+		} catch (Exception e) {
+			e.printStackTrace(); // 输出异常信息
+		}
+	}
+
+	public edit(Socket socket, String ins, int row, int column, int account_id) {
+		this.socket = socket;
+		this.instruction = ins;
+		this.account_id = account_id;
+		this.row = row;
+		this.column = column;
+
 		this.setSize(400, 500);
-		//set size for edit window
 		this.setLayout(null);
-		//Default layout style
 		this.setLocation(new Point(600, 200));
-		//set location for sign
 		setTitle("edit");
-		//set the title edit
-		
-		 /*creat some JLabel and set the size ,show the coordinate of the rectangle of each*/
 		course_name = new JLabel("Course :");
-		//display text course name
 		course_name.setBounds(65, 100, 100, 30);
-		//set the bound of the course name
 		classroom = new JLabel("classroom:");
-		//display text classroom
 		classroom.setBounds(65, 150, 100, 30);
-		//set the bound of the classroom
 		lecturer = new JLabel("lecturer:");
-		//display text lecturer
 		lecturer.setBounds(65, 200, 100, 30);
-		//set the bound of the lecturer
 		confirm = new JButton("confirm");
-		//display text confirm
 		confirm.setBounds(225, 300, 80, 30);
-		//set the bound of the confirm
 		exit = new JButton("Cancel");
-		//display text cancel
 		exit.setBounds(95, 300, 80, 30);
-		//set the bound of the cancel
 
 		name = new JTextField();
 		name.setBounds(140, 100, 120, 30);
-		//Allow edit single line user name text
 		location = new JTextField();
 		location.setBounds(140, 150, 120, 30);
-		//Allow edit single line location text
 		teacher = new JTextField();
 		teacher.setBounds(140, 200, 120, 30);
-		//Allow edit single line teacher text
 
 		this.add(course_name);
 		this.add(classroom);
@@ -87,15 +81,63 @@ public class edit extends JFrame {
 		this.add(location);
 		this.add(teacher);
 
-		//add listener
-		listenerForEdit myaction = new listenerForEdit(this);
-		confirm.addMouseListener(myaction);
-		exit.addMouseListener(myaction);
+		confirm.addActionListener(new ActionListener() {
+			public void actionPerformed(final ActionEvent e) {
+				writer.println("Edit");
+				writer.println(instruction);
+				writer.println(name.getText());
+				writer.println(location.getText());// 将文本框中信息写入流
+				writer.println(teacher.getText());
+				writer.println(account_id);
+				writer.println(row);
+				writer.println(column);
+				new CourseTable(socket);
+				dispose();
+			}
+		});
+		exit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					socket.close();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				dispose();
+			}
+		});
 
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//When the user clicks a button ,close the window
 		this.setVisible(true);
-		//Enable visualization of the table
+		connect();
 	}
 
+	boolean isrun = true;
+
+	@Override
+	public void run() {
+		try {
+			while (isrun) { // 如果套接字是连接状态
+				if (reader != null) {
+					String line = reader.readLine();// 读取服务器发送的信息
+					// if change success
+					if (line.equals("success"))
+						this.dispose();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (reader != null) {
+					reader.close();// 关闭流
+				}
+				if (socket != null) {
+					socket.close(); // 关闭套接字
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
